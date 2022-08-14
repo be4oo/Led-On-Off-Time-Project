6a446b8c91ef333da51c6782a4dd4de7ce6471b1
Beshoy Wesa <besho.ageeb@yahoo.com>
2022-08-14 22:23:40 +0200

    Delete LedOnOff_Beshoy_v1/LedTest6 directory

--- LedOnOff_Beshoy_v1/LedTest6/Objects/dio_program.d
+++
@@ -1,6 +0,0 @@
-./objects/dio_program.o: \
-  ..\..\04-\ System\02-\ Mcal\01-\ DIO_v2\DIO_Program.c \
-  ..\..\04-\ System\05-\ Lib\STD_TYPES.h \
-  ..\..\04-\ System\05-\ Lib\BIT_MATH.h \
-  ..\..\04-\ System\02-\ Mcal\01-\ DIO_v2\DIO_Int.h \
-  ..\..\04-\ System\02-\ Mcal\01-\ DIO_v2\DIO_Private.h
